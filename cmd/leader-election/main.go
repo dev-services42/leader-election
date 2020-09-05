cc6978e344fa1ee2d2def8ad20ee66bb10ca1a86
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2020-09-06 00:46:32 +0300

    ++

--- cmd/leader-election/main.go
+++ cmd/leader-election/main.go
@@ -2,68 +2,79 @@ package main
 
 import (
 	"context"
+	"fmt"
 	election "github.com/dev-services42/leader-election-lib/leader-election"
 	"github.com/dev-services42/leader-election-lib/leader-election/keys"
 	"github.com/dev-services42/leader-election-lib/leader-election/sessions"
+	config2 "github.com/dev-services42/leader-election/config"
 	"github.com/dev-services42/leader-election/domain"
 	"github.com/dev-services42/leader-election/facade"
 	"github.com/hashicorp/consul/api"
+	"github.com/urfave/cli"
 	"go.uber.org/zap"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
-const (
-	grpcAddr = ":8000"
-	// disconnect slow clients
-	slowClientReadTTL = time.Second
-	consulAddr        = "127.0.0.1:8500"
-	sessionTTL        = 60 * time.Second
-	keysRecheckTTL    = 10 * time.Second
-	sessionName       = "services/leader-election/leader"
-	keyName           = sessionName
-)
-
-func checkErr(logger *zap.Logger, err error) {
-	if err != nil {
-		logger.Fatal("error during startup", zap.Error(err))
-	}
-}
-
-func main() {
+func cmdRun(c *cli.Context) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	logger, err := zap.NewDevelopment()
 	if err != nil {
-		panic(err)
+		return err
+	}
+
+	configFile := c.String("config")
+	if configFile == "" {
+		configFile = "config.toml"
+	}
+
+	cfg, err := config2.Parse(configFile)
+	if err != nil {
+		return err
 	}
 
 	config := api.DefaultConfig()
-	config.Address = consulAddr
+	config.Address = cfg.Consul.Addr
 	consul, err := api.NewClient(config)
-	checkErr(logger, err)
+	if err != nil {
+		return err
+	}
 
 	srvSessions, err := sessions.New(logger, consul)
-	checkErr(logger, err)
+	if err != nil {
+		return err
+	}
 
-	srvKeys, err := keys.New(logger, consul, keysRecheckTTL)
-	checkErr(logger, err)
+	srvKeys, err := keys.New(logger, consul, cfg.Consul.GetKeyRecheckTTL())
+	if err != nil {
+		return err
+	}
 
-	srvLeaderElection, err := election.New(logger, consul, srvSessions, srvKeys, sessionTTL, sessionName, keyName)
-	checkErr(logger, err)
+	srvLeaderElection, err := election.New(logger, consul, srvSessions, srvKeys, cfg.Consul.GetSessionTTL(), cfg.Consul.SessionName, cfg.Consul.KeyName)
+	if err != nil {
+		return err
+	}
 
-	srvDomain, err := domain.New(logger, srvLeaderElection, slowClientReadTTL)
-	checkErr(logger, err)
+	srvDomain, err := domain.New(logger, srvLeaderElection, cfg.GRPC.GetSlowClientReadTTL())
+	if err != nil {
+		return err
+	}
 
-	checkErr(logger, srvDomain.Run(ctx))
+	if err := srvDomain.Run(ctx); err != nil {
+		return err
+	}
 
-	srvFacade, err := facade.New(logger, grpcAddr, srvDomain)
-	checkErr(logger, err)
+	srvFacade, err := facade.New(logger, cfg.GRPC.Listen, srvDomain)
+	if err != nil {
+		return err
+	}
 
-	checkErr(logger, srvFacade.Run(ctx))
+	if err := srvFacade.Run(ctx); err != nil {
+		return err
+	}
 
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
@@ -76,4 +87,30 @@ func main() {
 
 	srvFacade.Wait()
 	srvDomain.Wait()
+
+	return nil
+}
+
+func main() {
+	a := cli.NewApp()
+	a.Name = "Leader Election"
+	a.Usage = "Leading election for every pod in simple way"
+	a.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config,c",
+			Value: "config.toml",
+			Usage: "Config file path",
+		},
+	}
+	a.Commands = []cli.Command{
+		{
+			Name:   "run",
+			Usage:  "Run leader election process",
+			Action: cmdRun,
+		},
+	}
+
+	if err := a.Run(os.Args); err != nil {
+		fmt.Println("cannot exec app command", err)
+	}
 }
